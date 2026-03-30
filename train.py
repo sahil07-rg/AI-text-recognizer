048606dbe13b3e5786675bad5b91287c22bc7830
sahil07-rg <sahilkumarjnv12@gmail.com>
2026-03-30 22:02:29 +0530

    Create training script for DistilBERT
    Add a placeholder training script for DistilBERT.

---
+++ train.py
@@ -0,0 +1,6 @@
+# Training script (summary version)
+
+from transformers import Trainer, TrainingArguments
+# Full training done on Kaggle with DistilBERT
+
+print("Training script placeholder - see notebook for full training")
